281d418540693797a67199bd2269a7156107ccd4
Diêgo Sousa <34814235+DiegoSousa123@users.noreply.github.com>
2024-01-14 21:39:37 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
+let cNav = document.body.querySelector(".c-nav");
+let hamburger = document.body.querySelector(".js-hamburger");
 
+hamburger.addEventListener("click", function () {
+	let hamburgerAttr = hamburger.getAttribute("aria-expanded") === "false";
+	hamburger.setAttribute("aria-expanded", `${hamburgerAttr}`);
+	console.log(hamburgerAttr);
+	menuState();
+});
+cNav.onclick = function () {menuState();}
+function menuState(){
+	cNav.classList.toggle("c-nav--visible");
+	hamburger.classList.toggle('js-hamburger--active');
+}
+ 
